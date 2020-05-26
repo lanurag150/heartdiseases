7a4f8c6ef854e4abdf81796f33e74d9ed35822ca
lanurag150 <60335727+lanurag150@users.noreply.github.com>
2020-05-26 11:33:24 +0530

    Update index.php

--- templates/index.php
+++ templates/index.php
@@ -11,8 +11,8 @@
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <link rel="stylesheet" href="{{ url_for('static', filename='css/open-iconic-bootstrap.min.css') }}" >
-    <link rel="stylesheet"  href="{{ url_for('static', filename='css/animate.css') }}>
+       <link rel="stylesheet" href="{{ url_for('static', filename='css/open-iconic-bootstrap.min.css') }}" >
+    <link rel="stylesheet"  href="{{ url_for('static', filename='css/animate.css') }}">
     
     <link rel="stylesheet" href="{{ url_for('static', filename='css/owl.carousel.min.css') }}" >
     <link rel="stylesheet" href="{{ url_for('static', filename='css/owl.theme.default.min.css') }}" >
@@ -26,7 +26,7 @@
     <link rel="stylesheet" href="{{ url_for('static', filename='css/jquery.timepicker.css') }}">
 
     <link rel="stylesheet" href="{{ url_for('static', filename='css/style.css') }}" >
-    <link rel="stylesheet"href="{{ url_for('static', filename='css/flaticon.css') }}" >
+    <link rel="stylesheet" href="{{ url_for('static', filename='css/flaticon.css') }}" >
     <link rel="stylesheet" href="{{ url_for('static', filename='css/icomoon.css') }}">
   </head>
   <body>
@@ -61,17 +61,7 @@
   </nav>
     <!-- END nav -->
     
-    <div class="hero-wrap" style="background-image: url('images/cardio.jpg'); background-attachment:fixed;">
-      <div class="overlay"></div>
-      <div class="container">
-        <div class="row no-gutters slider-text align-items-center justify-content-center" data-scrollax-parent="true">
-          <div class="col-md-8 ftco-animate text-center">
-            <p class="breadcrumbs"><span class="mr-2"><a href="http://premediccare.rf.gd/index.php">Home</a></span></p>
-            <h3 class="mb-3 bread">CardioVascular-Department</h3>
-          </div>
-        </div>
-      </div>
-    </div>
+   
 
    <section class="ftco-section contact-section ftco-degree-bg" style="height: 1500px">
    
@@ -148,7 +138,7 @@
             </div>
       {{ prediction_text }}
     </section>
-		<footer class="ftco-footer ftco-bg-dark ftco-section img" style="background-image: url(images/bg_5.jpg);">
+		
     	<div class="overlay"></div>
       <div class="container">
         <div class="row mb-5">
@@ -198,40 +188,35 @@
           </div>
         </div>
       </div>
-    </footer>
+   
     
   
 
   <!-- loader -->
   <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
 
- <!-- Jquery JS-->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <!-- Vendor JS-->
-    <script src="vendor/select2/select2.min.js"></script>
-    <script src="vendor/datepicker/moment.min.js"></script>
-    <script src="vendor/datepicker/daterangepicker.js"></script>
+
 
     <!-- Main JS-->
-    <script src="js/global.js"></script>
+   
 
-  <script src="js/jquery.min.js"></script>
-  <script src="js/jquery-migrate-3.0.1.min.js"></script>
-  <script src="js/popper.min.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-  <script src="js/jquery.easing.1.3.js"></script>
-  <script src="js/jquery.waypoints.min.js"></script>
-  <script src="js/jquery.stellar.min.js"></script>
-  <script src="js/owl.carousel.min.js"></script>
-  <script src="js/jquery.magnific-popup.min.js"></script>
-  <script src="js/aos.js"></script>
-  <script src="js/jquery.animateNumber.min.js"></script>
-  <script src="js/bootstrap-datepicker.js"></script>
-  <script src="js/jquery.timepicker.min.js"></script>
-  <script src="js/scrollax.min.js"></script>
+  <script src={{ url_for('static', filename='js/jquery.min.js') }}></script>
+  <script src={{ url_for('static', filename='js/jquery-migrate-3.0.1.min.js') }}></script>
+  <script src={{ url_for('static', filename='js/popper.min.js') }}></script>
+  <script src={{ url_for('static', filename='js/bootstrap.min.js') }}></script>
+  <script src={{ url_for('static', filename='js/jquery.easing.1.3.js') }}></script>
+  <script src={{ url_for('static', filename='js/jquery.waypoints.min.js') }}></script>
+  <script src={{ url_for('static', filename='js/jquery.stellar.min.js') }}></script>
+  <script src={{ url_for('static', filename='js/owl.carousel.min.js') }}></script>
+  <script src={{ url_for('static', filename='js/jquery.magnific-popup.min.js') }}></script>
+  <script src={{ url_for('static', filename='js/aos.js') }}></script>
+  <script src={{ url_for('static', filename='js/jquery.animateNumber.min.js') }}></script>
+  <script src={{ url_for('static', filename='js/bootstrap-datepicker.js') }}></script>
+  <script src={{ url_for('static', filename='js/jquery.timepicker.min.js') }}></script>
+  <script src={{ url_for('static', filename='js/scrollax.min.js') }}></script>
   
-  <script src="js/google-map.js"></script>
-  <script src="js/main.js"></script>
+  <script src={{ url_for('static', filename='js/google-map.js') }}></script>
+  <script  src={{ url_for('static', filename='js/main.js') }}></script>
     
   </body>
 </html>
