8c143c5a07003f701dc23e0dc723d974105f1eae
lanurag150 <60335727+lanurag150@users.noreply.github.com>
2020-05-27 15:33:40 +0530

    Update index.php

--- templates/index.php
+++ templates/index.php
@@ -73,7 +73,7 @@
       </div>
     </div>
 
-   <section class="ftco-section contact-section ftco-degree-bg" style="height: 3500px">
+   <section class="ftco-section contact-section ftco-degree-bg" style="height: 1500px">
    
       <div class="container col-lg-6" >
           
