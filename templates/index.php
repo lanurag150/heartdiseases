100e1c7238a2184f29f7423f3a70462e513b86e7
lanurag150 <60335727+lanurag150@users.noreply.github.com>
2020-05-30 12:15:28 +0530

    Update index.php

--- templates/index.php
+++ templates/index.php
@@ -78,7 +78,7 @@
       <div class="container col-lg-6" >
           
           
-            <div class="w3-card-4" style="width: 550px;"style="background-color:White">
+            <div class="w3-card-4" style="width: 550px;background-color:White">
               <div class="w3-container w3-black">
                 <h2 style="color:white;">Heart Disease Analysis</h2>
               </div>
